3e00494d1247fccc0890d9b7f9c94a4e94f40d76
venkat1817 <dvenkataramir@gmail.com>
2023-05-16 12:49:46 +0530

    dynamo-python-ec2

--- dynamo.py
+++ dynamo.py
@@ -24,38 +24,54 @@
 
   #app.py
 
+
+#python3 app.py run
+
+#output give me ip address in browser
+# ex:-http://18.119.12.40:8001/ 
+
 from flask import Flask, render_template
 import boto3
 
 app = Flask(__name__, template_folder='template')
 
 
-dynamodb = boto3.client('dynamodb', region_name='us-east-2')
+# Create a DynamoDB client with the specified region
+dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+
 @app.route('/')
 def index():
+    # Query the DynamoDB table
     response = dynamodb.scan(TableName='dynamodb-1')
     items = response['Items']
+
+    # Convert DynamoDB items to a list of dictionaries
     data = []
     for item in items:
         data.append({
-            'column1': item['column']['S'],
-            'column2': item['column2']['S'],
-            'column3': item['column3']['N'],
-            'column4': item['column4']['N']
-
-
+            'name': item['name']['S'],
+            'age': item['age']['N'],
+            'college': item['college']['S'],
+            'location': item['location']['S']
         })
 
-   
-    return render_template('table.html', data=data)
+    # Add additional rows of data manually
+    data.append({
+        'name': 'venkat',
+        'age': '22',
+        'college': 'Sv University',
+        'location': 'tirupati'
+    })
 
+    data.append({
+        'name': 'sathish',
+        'age': '20',
+        'college': 'CV raman jr College',
+        'location': 'anantapur'
+    })
+
+    # Render the template with the data
+    return render_template('table.html', data=data)
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=8001, debug=True)
-#python3 app.py run
-
-#output give me ip address in browser
-# ex:-http://18.119.12.40:8001/ 
-
-
-
 
